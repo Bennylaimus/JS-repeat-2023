48f1f3a078a336362209035388e9ece653206d6e
Andrew Komorkovich <komorkovics@gmail.com>
2023-10-08 23:02:07 +0300

    array and array methods. 3 of 33 array methods repeated

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
+// forEach
+// map
+// filter
 
+// ==========================
+
+// concat
+// find
+// findIndex
+// indexOf
+// lastIndexOf
+// some
+// every
+// includes
+// push
+// unshift
+// pop
+// shift
+// toString
+// join
+// fill
+// copyWithin
+// slice
+// splice
+// sort
+// reverse
+// from
+// isArray
+// valueOf
+// entries
+// keys
+// values
+// reduce
+// reduceRight
+// flat
+// flatMap
+
+let food = ['Banana', 'Potato', 'Carrot', 'Orange', 'Kiwi', 'Apple', 'Melon'];
+
+const filtredFood = food.filter((item) => {
+    if (item.length !== 6) {
+        return item
+    }
+});
+
+console.log(filtredFood);
+
+https://www.youtube.com/watch?v=RVxuGCWZ_8E&t=171s
