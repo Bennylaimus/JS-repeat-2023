42c1dc53230addf6c45febbd8db2cfae8a06780e
Andrew Komorkovich <komorkovics@gmail.com>
2023-10-15 10:59:06 +0300

    repeat of JS Objects (part 1)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=RVxuGCWZ_8E&t=171s
+
 // forEach
 // map
 // filter
@@ -34,17 +36,109 @@
 
 // ==========================
 
-let food = ['Cucumber', 'Potato', 'Carrot', 'Orange', 'Kiwi', 'Potato', 'Apple', 'Melon'];
+// let user = {
+//     name: 'John',
+//     age: 30,
+//     passed: true,
+// };
 
-const numbers = [1, 2, 3, 5, 80, 90, 33, 35];
+// user.isAdmin = false;
+// user.adress = 'Kronvalda street 1-2';
+// user["have permission"] = true;
 
-const multiDemArr = [1, 2, 3, [4, 5, [6, [7]]]];
+// delete user.isAdmin;
 
-let flatted = multiDemArr.flatMap((element) => {
-    return element[0] + element[0]
-});
+// console.log(user);
+// console.log(user["have permission"]);
 
-console.log(flatted);
+// ====================================
 
+// let key = prompt('What info would you like to know?', 'name');
 
-// https://www.youtube.com/watch?v=RVxuGCWZ_8E&t=171s
+// console.log(user[key]);
+
+// =====================================
+
+// let fruits = prompt('What fruits are you bought?', 'melon');
+
+// let bag = {
+//     place: 'Wallmart',
+//     address: 'Kinsington street 1-2, New Hamshire',
+//     isOpen: true,
+//     [fruits]: 5,
+//     [fruits + 'Computers']: 15,
+// };
+
+// console.log(bag.melonComputers); // 5
+
+// ======================================
+
+// let createUser = function (name, age, address) {
+//     return {
+//         name: name,
+//         age: age,
+//         address: address,
+//     }
+// };
+
+// let user = createUser('Andrew', 30, 'Kronvalda street 1-2');
+
+// console.log(user);
+
+// let newUser = function (name, age, isAdmin) {
+//     return {
+//         name,
+//         age: 30,
+//         isAdmin,
+//     }
+// }
+
+// let user = newUser('Andrew', 30, true);
+
+// console.log(user);
+
+// console.log('age' in user); // true
+
+// let placeOfLiving = "address";
+
+// console.log(placeOfLiving in user); // true
+
+// ============================================================
+
+// let createUser = function (name, age, address) {
+//     return {
+//         name: name,
+//         age: age,
+//         address: address,
+//     }
+// };
+
+// let user = createUser('Andrew', 30, 'Kronvalda street 1-2');
+
+// console.log(user);
+
+// for (let keyValue in user) {
+//     console.log(keyValue, ':', user[keyValue]);
+// };
+
+// let user = {}; // or let user = new Object();
+// user.name = 'John';
+// user.surname = 'Smith';
+// user.name = 'Pete';
+// delete user.name
+
+// console.log(user);
+
+let schedule = {
+    name: 'Pete',
+};
+
+function isEmpty(objectName) {
+    for (let key in objectName) {
+        if (objectName[key] === 'undefined') {
+            return true
+        } false
+    }
+}
+
+console.log(isEmpty(schedule));
