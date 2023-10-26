163ddd05d67637bcd944d9a4fb53a56290bb1009
Andrew Komorkovich <komorkovics@gmail.com>
2023-10-26 23:38:40 +0300

    x1 kata solved (7 kye)

---
+++ codewars.js
@@ -0,0 +1,25 @@
+// Codewars task Nr.1
+// My solution
+
+// let isSquare = function (integer) {
+//     let squareRoot = Math.sqrt(integer);
+//     if (integer < -1) {
+//         return false;
+//     } else if (integer === 0) {
+//         return true;
+//     } else if (Math.trunc((integer / squareRoot)) == squareRoot) {
+//         return true;
+//     } return false;
+// };
+
+// console.log(isSquare(25));
+
+// // Best solution:
+
+// function isSquare(n) {
+//     return Math.sqrt(n) % 1 === 0;
+// };
+
+// ===============================================================================
+
+console.log(25 % 1);
